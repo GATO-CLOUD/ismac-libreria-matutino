778c1ae3e905988d969611c048d558672e10b7d3
AG <AG@LAB-SW3-09.ALUMNOS.LOCAL>
2024-11-15 11:24:17 -0500

    segunda parte creación de AutorPrincipal

--- src/com/distribuida/principal/AutorPrincipal.java
+++ src/com/distribuida/principal/AutorPrincipal.java
@@ -5,7 +5,7 @@ import com.distribuida.entities.Autor;
 public class AutorPrincipal {
  
     public static void main(String[] args) {
-        
+        //clase principal autor para gestionar la base de datos.
         Autor autor = new Autor(1, "Jaimito", "Pozo", "Panama", "Calle bonita", "584021548", "Moon@mail.com");
        
     
